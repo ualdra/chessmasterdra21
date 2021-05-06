f4b5ac926ec31b178ac63ab743251b7cd6aadfd9
Juan José Pallarés Sánchez <jps105@inlumine.ual.es>
2021-05-06 18:00:31 +0200

    Change rails to api only

--- backend/Oauth2Server/app/controllers/sessions_controller.rb
+++ backend/Oauth2Server/app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
     def login
-        debugger
         user = User.find_by_name(params[:name]) if params[:name].present? || params[:password].present?
         user&.refresh_token
         
